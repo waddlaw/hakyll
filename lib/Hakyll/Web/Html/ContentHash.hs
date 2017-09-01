004f356c28a3eb92a8ec4077f30e33a925a75ca7
waddlaw <ingronze@gmail.com>
2017-09-01 16:39:25 +0900

    Add contentHashRoute

--- lib/Hakyll/Web/Html/ContentHash.hs
+++ lib/Hakyll/Web/Html/ContentHash.hs
@@ -1,12 +1,15 @@
 module Hakyll.Web.Html.ContentHash
   ( contentHashUrls
   , contentHashCompiler
+  , contentHashRoute
   ) where
 
-import qualified Data.ByteString.Lazy as LB (readFile)
+import qualified Data.ByteString.Lazy as LB (readFile, ByteString)
 import Data.Digest.Pure.MD5 (md5)
 import Hakyll.Core.Compiler (Compiler, makeItem, unsafeCompiler)
+import Hakyll.Core.Identifier (Identifier, toFilePath)
 import Hakyll.Core.Item (Item, itemBody)
+import Hakyll.Core.Routes (Routes, customRouteIO)
 import Hakyll.Web.Html (withTagsM, isExternal)
 import Text.HTML.TagSoup (Tag(TagOpen))
 
@@ -22,7 +25,7 @@ contentHashUrls item = (withTagsM tag . itemBody $ item) >>= makeItem
     attr (k, v) = do
       hashItem <-
         if (k `elem` ["src"]) && (not $ isExternal v)
-          then contentHashCompiler [v]
+          then contentHashCompiler v
           else makeItem v
       return (k, itemBody hashItem)
 
@@ -31,10 +34,33 @@ contentHashUrls item = (withTagsM tag . itemBody $ item) >>= makeItem
 -- 与えられたパスのハッシュを計算する
 -- 常に引数は1つ
 --
-contentHashCompiler :: [FilePath] -> Compiler (Item String)
+contentHashCompiler :: FilePath -> Compiler (Item String)
 contentHashCompiler path = do
-  let ext = getExt $ head path
-  content <- unsafeCompiler $ LB.readFile $ head path
-  makeItem $ (\fn -> mconcat ["/images/", fn, ".", ext]) $ show $ md5 content
+  content <- unsafeCompiler . LB.readFile $ path
+  makeItem $ renderHashPath path content
+
+-- |
+--
+-- コンテンツハッシュルート
+--
+contentHashRoute :: Routes
+contentHashRoute = customRouteIO toHash
+
+-- |
+--
+-- ファイルの md5sum を基にファイルパスを生成
+--
+toHash :: Identifier -> IO FilePath
+toHash id' = do
+  let path = toFilePath id'
+  content <- LB.readFile path
+  return $ renderHashPath path content
+
+renderHashPath :: FilePath -> LB.ByteString -> String
+renderHashPath fp = render . show . md5
   where
-    getExt = reverse . takeWhile (/= '.') . reverse
+    render hv = mconcat ["images/", hv, ".", ext]
+    ext = getExt fp
+
+getExt :: String -> String
+getExt = reverse . takeWhile (/= '.') . reverse
